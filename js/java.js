add81f57e87a99b39bd87b6774c15012232a0e18
DJ Roychoudhury and Ryan Carlile <ryan@>
2016-03-22 11:38:33 -0700

    This will add jquery functions to terms

--- js/java.js
+++ js/java.js
@@ -1,30 +1,76 @@
 $(document).ready(function() {
-  // $("h1").dblclick(function() {
-  //   alert("This is a header.");
-  // });
-  $(".clickable").click(function() {
-    $("#Initially-hidden").slideToggle();
-    $("#Initially-showing").slideToggle();
-  });
-$(".clickable1").click(function() {
-    $("#local-hidden").fadeToggle();
-    $("#local-showing").fadeToggle();
+  $(".clickable1").click(function() {
+    $("#term1-hidden").slideToggle();
+    $("#term1-showing").slideToggle();
   });
 $(".clickable2").click(function() {
-    $("#bar-hidden").slideToggle();
-    $("#bar-showing").slideToggle();
+    $("#term2-hidden").fadeToggle();
+    $("#term2-showing").fadeToggle();
   });
-
-
-  // $("img").hover(function() {
-  //   alert("This is an image.");
-  // });
-  // $("h2").click(function() {
-  //   alert("This is a sub-header.");
-  // });
-  // $("ul").click(function() {
-  //   alert("This is a list.");
-  // });
+$(".clickable3").click(function() {
+    $("#term3-hidden").slideToggle();
+    $("#term3-showing").slideToggle();
+  });
+  $(".clickable4").click(function() {
+    $("#term4-hidden").slideToggle();
+    $("#term4-showing").slideToggle();
+  });
+$(".clickable5").click(function() {
+    $("#term5-hidden").fadeToggle();
+    $("#term5-showing").fadeToggle();
+  });
+$(".clickable6").click(function() {
+    $("#term6-hidden").slideToggle();
+    $("#term6-showing").slideToggle();
+  });
+  $(".clickable7").click(function() {
+    $("#term7-hidden").slideToggle();
+    $("#term7-showing").slideToggle();
+  });
+$(".clickable8").click(function() {
+    $("#term8-hidden").fadeToggle();
+    $("#term8-showing").fadeToggle();
+  });
+$(".clickable9").click(function() {
+    $("#term9-hidden").slideToggle();
+    $("#term9-showing").slideToggle();
+  });
+  $(".clickable10").click(function() {
+    $("#term10-hidden").slideToggle();
+    $("#term10-showing").slideToggle();
+  });
+$(".clickable11").click(function() {
+    $("#term11-hidden").fadeToggle();
+    $("#term11-showing").fadeToggle();
+  });
+$(".clickable12").click(function() {
+    $("#term12-hidden").slideToggle();
+    $("#term12-showing").slideToggle();
+  });
+  $(".clickable13").click(function() {
+    $("#term13-hidden").slideToggle();
+    $("#term13-showing").slideToggle();
+  });
+$(".clickable14").click(function() {
+    $("#term14-hidden").fadeToggle();
+    $("#term14-showing").fadeToggle();
+  });
+$(".clickable15").click(function() {
+    $("#term15-hidden").slideToggle();
+    $("#term15-showing").slideToggle();
+  });
+  $(".clickable16").click(function() {
+    $("#term16-hidden").slideToggle();
+    $("#term16-showing").slideToggle();
+  });
+$(".clickable17").click(function() {
+    $("#term17-hidden").fadeToggle();
+    $("#term17-showing").fadeToggle();
+  });
+$(".clickable18").click(function() {
+    $("#term18-hidden").slideToggle();
+    $("#term18-showing").slideToggle();
+  });
 });
 
 $(function(){
